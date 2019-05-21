5185e86400e3fb4fc2f5879fb824c5fc11279843
Benjamin Beyret <bb1010@ic.ac.uk>
2019-05-21 17:28:18 +0100

    remove rand_colors, replace with list of colors

--- animalai/envs/arena_config.py
+++ animalai/envs/arena_config.py
@@ -25,24 +25,39 @@ class Vector3(yaml.YAMLObject):
 
         return res
 
+class RGB(yaml.YAMLObject):
+    yaml_tag = u'!RGB'
+
+    def __init__(self, r=0, g=0, b=0):
+        self.r = r
+        self.g = g
+        self.b = b
+
+    def to_proto(self):
+        res = ArenaParametersProto.ItemsToSpawn.Vector3()
+        res.x = self.r
+        res.y = self.g
+        res.z = self.b
+
+        return res
+
 
 class Item(yaml.YAMLObject):
     yaml_tag = u'!Item'
 
-    def __init__(self, name='', rand_color=False, positions=None, rotations=None, sizes=None):
+    def __init__(self, name='', positions=None, rotations=None, sizes=None, colors = None):
         self.name = name
-        self.rand_color = rand_color
         self.positions = positions if positions is not None else []
         self.rotations = rotations if rotations is not None else []
         self.sizes = sizes if sizes is not None else []
+        self.colors = colors if colors is not None else []
 
 
 class Arena(yaml.YAMLObject):
     yaml_tag = u'!Arena'
 
-    def __init__(self, t=1000, rand_all_colors=False, items=None, blackouts=None):
+    def __init__(self, t=1000, items=None, blackouts=None):
         self.t = t
-        self.rand_all_colors = rand_all_colors
         self.items = items if items is not None else {}
         self.blackouts = blackouts if blackouts is not None else []
         self.generate_blackout_steps()
@@ -89,14 +104,13 @@ class ArenaConfig(yaml.YAMLObject):
         for k in self.arenas:
             config_out.arenas[k].CopyFrom(ArenaParametersProto())
             config_out.arenas[k].t = self.arenas[k].t
-            config_out.arenas[k].rand_all_colors = self.arenas[k].rand_all_colors
             for item in self.arenas[k].items:
                 to_spawn = config_out.arenas[k].items.add()
                 to_spawn.name = item.name
-                to_spawn.rand_color = item.rand_color
                 to_spawn.positions.extend([v.to_proto() for v in item.positions])
                 to_spawn.rotations.extend(item.rotations)
                 to_spawn.sizes.extend([v.to_proto() for v in item.sizes])
+                to_spawn.colors.extend([v.to_proto() for v in item.colors])
 
         return config_out
 
