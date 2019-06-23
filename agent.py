287f4b238c01927693390f604fd8fdb6d9e423ef
Benjamin Beyret <bb1010@ic.ac.uk>
2019-06-23 16:05:49 +0100

    reformat Agent.py

--- agent.py
+++ agent.py
@@ -1,17 +1,22 @@
 from animalai.envs.brain import BrainInfo
 
+
 class Agent(object):
 
-    def __init__(self, configuration_to_load: str):
-
+    def __init__(self):
         """
          Load your agent here and initialize anything needed
-        :param configuration_to_load: path to your model to load
         """
         pass
 
-    def step(self, brain_info: BrainInfo) -> list[float]:
+    def reset(self, t=250):
+        """
+        Reset is called before each episode begins
+        Leave blank if nothing needs to happen there
+        :param t the number of timesteps in the episode
+        """
 
+    def step(self, brain_info: BrainInfo) -> list[float]:
         """
         A single step the agent should take based on the current
         :param brain_info:  a single BrainInfo containing the observations and reward for a single step for one agent
@@ -22,13 +27,5 @@ class Agent(object):
 
         return self.action
 
-    def reset(self):
-        """
-        Reset is called after each episode ends
-        Leave blank if nothing needs to happen there
-        :return:
-        """
-
     def destroy(self):
-
         pass
