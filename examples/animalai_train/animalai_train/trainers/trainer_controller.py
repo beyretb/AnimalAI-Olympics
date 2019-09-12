1ac43e8fc723124a4333c914f2ec780467e35388
Benjamin Beyret <bb1010@ic.ac.uk>
2019-09-12 10:15:43 +0100

    update trainer_controller for curriculum

--- examples/animalai_train/animalai_train/trainers/trainer_controller.py
+++ examples/animalai_train/animalai_train/trainers/trainer_controller.py
@@ -180,7 +180,7 @@ class TrainerController(object):
             environment.
         """
         if self.meta_curriculum is not None:
-            return env.reset(config=self.meta_curriculum.get_config())
+            return env.reset(arenas_configurations=self.meta_curriculum.get_config())
         else:
             if self.update_config:
                 return env.reset(arenas_configurations=self.config)
