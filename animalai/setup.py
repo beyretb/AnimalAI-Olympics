a105d63636781c58ac9da490c2c3bc6036a3d291
Benjamin <bb1010@ic.ac.uk>
2020-04-23 10:26:56 +0100

    update requirements.txt and setup.py

--- animalai/setup.py
+++ animalai/setup.py
@@ -1,23 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="animalai",
     version="2.0.0b0",
-    description="Animal AI competition interface",
+    description="Animal AI envronment Python API",
     url="https://github.com/beyretb/AnimalAI-Olympics",
     author="Benjamin Beyret",
-    author_email="bb1010@ic.ac.uk",
+    author_email="beyretb@gmail.com",
     classifiers=[
         "Intended Audience :: Developers",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    packages=[
-        "animalai.envs",
-        "animalai.envs.gym",
-        "animalai.communicator_objects",
-    ],  # Required
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     zip_safe=False,
     install_requires=["mlagents-envs==0.15.0", "jsonpickle", "pyyaml"],
     python_requires=">=3.5",
--- animalai_train/setup.py
+++ animalai_train/setup.py
@@ -1,19 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="animalai_train",
     version="2.0.0b0",
-    description="Animal AI competition training library",
+    description="Animal AI training library",
     url="https://github.com/beyretb/AnimalAI-Olympics",
     author="Benjamin Beyret",
-    author_email="bb1010@ic.ac.uk",
+    author_email="beyretb@gmail.com",
     classifiers=[
         "Intended Audience :: Developers",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    packages=["animalai_train"],  # Required
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     zip_safe=False,
     install_requires=["animalai==2.0.0b0", "mlagents==0.15.0"],
     python_requires=">=3.6.1",
